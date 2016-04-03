f5cc8b6224f06efa3151a922ed8584d8d70a84c2
lelepyt <volodia9696@gmail.com>
2016-04-03 12:28:11 -0700

    I created the array of my objects and filled in the list with the help of this array. I have renamed the checkObject function to checkList function. CheckList function is now allowed to return boolean

--- org.exercises.listutils/ListUtils.java
+++ org.exercises.listutils/ListUtils.java
@@ -1,38 +1,38 @@
-package worklist;
+
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Worker {
+public class ListUtils {
 
 	private List<String> list;
 
 	public static void main(String[] args) {
 		int value = 1;
-		Worker worker = new Worker(value);
-		worker.addObject(new String("Volodya"));
-		worker.addObject(new String("Vasyl"));
-		worker.addObject(new String("Taras"));
-		worker.addObject(new String("Andriy"));
-		worker.addObject(new String("Yuriy"));
-		worker.checkObject("Taras");
-		worker.deleteObject(1);
-		worker.deleteObject("Taras");
-		worker.printObject();
+		String array[] = { "Volodya", "Vasyl", "Taras", "Andriy", "Yuriy" };
+		ListUtils ListUtils = new ListUtils(value);
+		ListUtils.addObject(array);
+		ListUtils.checkList("Taras");
+		ListUtils.deleteObject(1);
+		ListUtils.deleteObject("Taras");
+		ListUtils.printObject();
 	}
 
-	public void addObject(String name) {
-		list.add(name);
+	public void addObject(String array[]) {
+		for (int i = 0; i < array.length; i++) {
+			list.add(array[i]);
+		}
 	}
 
-	public void checkObject(String name) {
+	public boolean checkList(String name) {
 		boolean isPresent = list.contains(name);
 		if (isPresent) {
 			System.out.println("This string is already  in the list. ");
 		} else {
 			System.out.println("This string wasn't found in the list");
 		}
+		return isPresent;
 	}
 
 	public void printObject() {
@@ -47,7 +47,7 @@ public class Worker {
 		list.remove(index);
 	}
 
-	public Worker(int value) {
+	public ListUtils(int value) {
 		if (value == 1) {
 			list = new ArrayList();
 		} else {
