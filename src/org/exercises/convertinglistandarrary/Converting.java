7abe27ab85e27e394dfc4c1cedeaf502cda3f8ad
lelepyt <volodia9696@gmail.com>
2016-04-07 13:00:24 -0700

    This program convert a list to an array and vice versa.

---
+++ src/org/exercises/convertinglistandarrary/Converting.java
@@ -0,0 +1,20 @@
+package org.exercises.convertinglistandarrary;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Converting {
+
+	public static void main(String[] args) {
+
+		Integer[] array = { 1, 2, 3, 4, 5 };
+		List<Integer> list = Arrays.asList(array);
+		Integer[] array2 = (Integer[]) list.toArray();
+
+		System.out.println("Print list: " + list.toString());
+		System.out.print("Print array: ");
+		for (int i = 0; i < array2.length; i++) {
+			System.out.print(array2[i] + "  ");
+		}
+	}
+}
